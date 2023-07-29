dab8a636ca55fe2a4ccbcf1f5dbb1d89ca516f2c
lalanikw <lalanikw@gmail.com>
2023-07-29 13:25:09 -1000

    Hero.js file update

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,21 +1,21 @@
-import koko from "./koko.JPG";
+
 
 const Hero = () => {
     return (
         <div className="hiro-section">
             <h1>Welcome to Paradise</h1>
             
-            <div className="hiro-details grid-2">
-                <div className="hiro-details-left">
+            {/* <div className="hiro-details">
+                <div className="hiro-details-left"> */}
                     {/* <h3>Discover the magic of Hawaii, your ultimate resource for planning an extraordinary Hawaiian getaway. Whether you're seeking pristine beaches, breathtaking landscapes, vibrant culture, or thrilling outdoor adventures, we are here to turn your dreams into reality. 
                     </h3> */}
                     {/* <button>contact us</button> */}
-                </div>
+                {/* </div> */}
                 {/* <div className="hiro-details-right">
                     <button>Explore Hawaii</button>
                 </div> */}
 
-            </div>
+             {/* </div> */}
             
         </div>
     )

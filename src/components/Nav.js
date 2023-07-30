390ee84a9c273c3ce7946c95ba04f39aa77f884a
lalanikw <lalanikw@gmail.com>
2023-07-29 22:32:14 -1000

    adjustments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,17 @@
 import React from "react";
-import logo from "../logo-no-bg.png";
+import logo from "../pages/logo.png";
 import {Link} from 'react-router-dom';
 import '../App.css';
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <div className="navlogo">
-        {/* <img src={logo} alt="logo" width= "10%" /> */}
-      </div>
-      <div>
-        <ul className="topnav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Explore">Explore Hawaii</Link></li>
-          <li><Link to="/Login">ContactUs</Link></li>
-        </ul>
-      </div>
+    <nav className="nav grid-2">
+      <img src={logo} alt="logo"/>
+      <ul className="topnav">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Explore">Explore Hawaii</Link></li>
+        <li><Link to="/Login">ContactUs</Link></li>
+      </ul>
     </nav>
   );
 };

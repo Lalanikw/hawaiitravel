b8d6d89728e12a70915873398cb9cc939f520516
lalanikw <lalanikw@gmail.com>
2023-09-08 23:42:48 +0530

    adding App.js and App.css updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Footer from './components/footer';
 import Login from './pages/Login';
 import Explore from './pages/Explore';
 import Sites from './pages/Sites';
+import Food from './pages/Food';
+import Event from './pages/Event';
+import SSS from './pages/SSS';
 
 const App = () => {
   return (
@@ -16,6 +19,10 @@ const App = () => {
         <Route index element={<Home/>} />
         <Route path="Explore" element={<Explore/>}/>
         <Route path="Login" element={<Login/>}/>
+        <Route path="Sites" element={<Sites/>}/>
+        <Route path="Food" element={<Food/>}/>
+        <Route path="Event" element={<Event/>}/>
+        <Route path="SSS" element={<SSS/>}/>
         <Route path="Sites" element={<Sites/>}/>
     </Routes>
     <Footer/>

fa6b701c07566b8d176426d0b50a1b46724bdb2f
lalanikw <lalanikw@gmail.com>
2023-10-07 12:49:59 +0530

    adjusting images to optmz speed

--- src/components/CarouselData.js
+++ src/components/CarouselData.js
@@ -3,7 +3,7 @@ import koko from "../pages/Sites/koko.JPG";
 import Waikiki from "../pages/Sites/Waikiki.JPG";
 import hanauma from "../pages/Sites/hanauma.jpg";
 import surf from "../pages/Sites/surf.JPG";
-import sunset from "../pages/Sites/sunset.JPG";
+// import sunset from "../pages/Sites/sunset.JPG";
 
 
 export const images = [
@@ -11,6 +11,6 @@ export const images = [
     {title:"Koko Crater", subtitle:"A challenging hike with rewarding panoramic views", img:koko},
     {title:"Waikiki Skyline", subtitle:"View of the Waikiki town from Diamon Head crater", img:Waikiki},
     {title:"Hanauma Bay", subtitle:"Perfect spot for Snorkeling, swimming and diving.", img:hanauma},
-    {title:"Surfer", subtitle:"Making friends with waves", img:surf},
-    {title:"Sunset at the Beach", subtitle:"Sunset Cruising in Waikiki Beach", img:sunset}
+    {title:"Surfer", subtitle:"Making friends with waves", img:surf}
+    
 ]
--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -11,7 +11,7 @@ function ChatBot() {
           chat-title="Hana"
           agent-id="de44beef-1d2f-4a54-8051-a7f20efc8cad"
           language-code="en"
-        ></df-messenger>
+      ></df-messenger>
     </div>
   );
 }

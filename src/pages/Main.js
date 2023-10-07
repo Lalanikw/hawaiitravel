2fc65af0759aeb0cf44cc94951cd440914a5ecc5
lalanikw <lalanikw@gmail.com>
2023-10-07 11:55:37 +0530

    adjusting images to optmz speed

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,7 +9,6 @@ const Main = () => {
                 </div>
                 <h1>Dreaming of a vacation in paradise?</h1>
                 <div className='main-section-right'>
-                    
                     <p>Travel to Hawaii and immerse yourself in the beauty of its stunning beaches, lush landscapes, and vibrant culture.</p>
                     <p>Get ready for a tropical adventure like no other!</p>
                     <p></p>
